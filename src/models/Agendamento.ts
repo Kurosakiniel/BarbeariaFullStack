a0b120d76551b8e0b18cde46c6248ececde87e91
Kurosakiniel <ichigoniel@gmail.com>
2026-06-02 11:23:46 -0300

    Banco de Dados Conectado e models pronto

---
+++ src/models/Agendamento.ts
@@ -0,0 +1,37 @@
+import { Schema, model, models } from "mongoose";
+
+const AgendamentoSchema = new Schema(
+  {
+    cliente: {
+      type: String,
+      required: true,
+    },
+
+    servico: {
+      type: String,
+      required: true,
+    },
+
+    data: {
+      type: String,
+      required: true,
+    },
+
+    horario: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["agendado", "concluido", "cancelado"],
+      default: "agendado",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default models.Agendamento ||
+  model("Agendamento", AgendamentoSchema);
